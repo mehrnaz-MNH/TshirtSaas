0ad681ea235613b94d0b65f4232a46a7fefc08f3
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-01-29 15:31:04 +1000

    s3 : Home page + animation

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { motion , AnimatePresence } from 'framer-motion';
+import {useSnapshot } from 'valtio';
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion'
+
+import state from '../store';
+
+import { CustomButton } from '../components';
 
 const Home = () => {
+
+  const snap = useSnapshot(state);
   return (
-    <div>Home</div>
+    <AnimatePresence>
+        {snap.intro &&(
+            <motion.section className="home" {...slideAnimation('left')}>
+               <motion.header {...slideAnimation('down')}>
+                <img
+                  src='./threejs.png'
+                  alt="logo"
+                  className="w-8 h-8 object-contain"
+                />
+               </motion.header>
+               <motion.div className="home-content" {...headContainerAnimation} >
+                   <motion.div {...headTextAnimation}>
+                      <h1 className="head-text">
+
+                        LET'S <br className="xl:block hidden" /> DO IT;
+
+                      </h1>
+                   </motion.div>
+                   <motion.div {...headContentAnimation}
+                     className="flex flex-col gap-5"
+                   >
+                    <p className = "max-w-md font-normal text-grey-600 text-base">
+                        Let's unleash your creativity buy using our brand-new 3D customizing tool
+                    </p>
+                    <CustomButton
+                        type="filled"
+                        title="Customized It"
+                        handleClick={() => state.intro = false}
+                        customStyles = "w-fit px-4 py-2.5 font-bold text-sm"
+                    />
+                   </motion.div>
+               </motion.div>
+            </motion.section>
+        )}
+    </AnimatePresence>
   )
 }
 
